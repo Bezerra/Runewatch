6d31d6589f8f2ea13b62ed6df2a612ee2509430e
pmarques93 <pmarques93@gmail.com>
2021-10-14 22:02:12 +0100

    Add bezerra spell spawner.

---
+++ Arcane Ascension/Assets/Scripts/TEMPSTUFF/BezerraDreamSpellSpawner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezerraDreamSpellSpawner : MonoBehaviour
+{
+    [SerializeField] private SpellSO spell;
+    [SerializeField] private float timeToSpawn;
+
+    private Character character;
+    private Stats characterStats;
+    private SpellBehaviourAbstract spellBehaviour;
+
+    private void Awake()
+    {
+        character = FindObjectOfType<Player>();
+        characterStats = character.GetComponent<Stats>();
+    }
+
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timeToSpawn);
+
+            GameObject spawnedSpell =
+            SpellPoolCreator.Pool.InstantiateFromPool(
+                spell.Name, transform.position,
+                Quaternion.LookRotation(transform.forward, transform.up));
+
+            // Gets behaviour of the spawned spell. Starts the behaviour and passes whoCast object (stats) to the behaviour.
+            spellBehaviour = spawnedSpell.GetComponent<SpellBehaviourOneShot>();
+            spellBehaviour.WhoCast = characterStats;
+
+            // Moves the spell forward
+            spawnedSpell.GetComponent<Rigidbody>().velocity = transform.forward * spell.Speed;
+        }
+    }
+}
