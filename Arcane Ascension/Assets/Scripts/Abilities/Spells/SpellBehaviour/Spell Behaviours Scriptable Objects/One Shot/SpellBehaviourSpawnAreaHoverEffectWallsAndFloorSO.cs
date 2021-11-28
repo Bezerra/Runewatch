41ccd2794534aae3fc9243949a9d2b3a658f9e09
pmarques93 <pmarques93@gmail.com>
2021-11-28 21:29:45 +0000

    Fix area target size on enemies.

--- Arcane Ascension/Assets/Scripts/Abilities/Spells/SpellBehaviour/Spell Behaviours Scriptable Objects/One Shot/SpellBehaviourSpawnAreaHoverEffectWallsAndFloorSO.cs
+++ Arcane Ascension/Assets/Scripts/Abilities/Spells/SpellBehaviour/Spell Behaviours Scriptable Objects/One Shot/SpellBehaviourSpawnAreaHoverEffectWallsAndFloorSO.cs
@@ -52,6 +52,9 @@ sealed public class SpellBehaviourSpawnAreaHoverEffectWallsAndFloorSO : SpellBeh
                             SpellAreaHoverPoolCreator.Pool.InstantiateFromPool(
                             parent.Spell.Name, DISTANTVECTOR,
                             Quaternion.identity);
+
+                        if (parent.AreaHoverVFX.TryGetComponent(out AreaTargetSizeUpdate targetSize))
+                            targetSize.UpdateAreaTargetSize(parent.Spell.AreaOfEffect);
                     }
 
                     parent.AreaHoverAreaHit = floorHit;
