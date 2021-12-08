7671281d4fb26536e3b498a9d70505b475925d63
pmarques93 <pmarques93@gmail.com>
2021-12-08 12:05:44 +0000

    Fix final boss spawn.

--- Arcane Ascension/Assets/Scripts/Level Generation/Game Progress Generation/PlayerTriggerRoomEnemySpawn.cs
+++ Arcane Ascension/Assets/Scripts/Level Generation/Game Progress Generation/PlayerTriggerRoomEnemySpawn.cs
@@ -15,7 +15,7 @@ public class PlayerTriggerRoomEnemySpawn : MonoBehaviour
             if (other.gameObject.layer == Layers.RoomProgressLayerNum)
             {
                 if (other.TryGetComponentInParent(
-                    out LevelPieceGameProgressControlNormalRoom progressControl))
+                    out LevelPieceGameProgressControlAbstract progressControl))
                 {
                     progressControl.SpawnEnemies();
                 }
