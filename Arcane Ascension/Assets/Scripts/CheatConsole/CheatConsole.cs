7f2e4dd507102cc0a168554c1a10dbcff686aa6a
pmarques93 <pmarques93@gmail.com>
2021-12-08 18:13:59 +0000

    Fix spells canvas.

--- Arcane Ascension/Assets/Scripts/CheatConsole/CheatConsole.cs
+++ Arcane Ascension/Assets/Scripts/CheatConsole/CheatConsole.cs
@@ -312,7 +312,8 @@ public class CheatConsole : MonoBehaviour, IFindPlayer, IFindInput
     {
         playerSpells.RemoveSpell(number2 - 1);
         playerSpells.AddSpell(spell, number2 - 1);
-        FindObjectOfType<PlayerHandEffect>().UpdatePlayerHandEffect(playerSpells.ActiveSpell);
+        playerSpells.SelectSpell(playerSpells.CurrentSpellIndex);
+        //FindObjectOfType<PlayerHandEffect>().UpdatePlayerHandEffect(playerSpells.ActiveSpell);
     }
 
     #endregion
