da6aa610bc859d8ef6ec2d8261b1ecf477410861
pmarques93 <pmarques93@gmail.com>
2022-01-18 17:14:14 +0000

    Add leaf tornado spell to natura small golem one shot release.

--- Arcane Ascension/Assets/Scripts/Abilities/Spells/SpellBehaviour/Spell Behaviours Scriptable Objects/SpellBehaviourSetProjectileOnFloorSO.cs
+++ Arcane Ascension/Assets/Scripts/Abilities/Spells/SpellBehaviour/Spell Behaviours Scriptable Objects/SpellBehaviourSetProjectileOnFloorSO.cs
@@ -9,7 +9,16 @@ sealed public class SpellBehaviourSetProjectileOnFloorSO : SpellBehaviourAbstrac
 {
     public override void StartBehaviour(SpellBehaviourOneShot parent)
     {
-        // Left blank on purpose
+        // Sets the projectile close to the floor
+        if (Physics.Raycast(
+            new Ray(parent.transform.position + Vector3.up, Vector3.down),
+            out RaycastHit floorHit, 10f, Layers.WallsFloor))
+        {
+            parent.transform.position = new Vector3(
+                parent.transform.position.x,
+                floorHit.point.y + floorHit.normal.y * 0.2f,
+                parent.transform.position.z);
+        }
     }
 
     public override void ContinuousUpdateBeforeSpellBehaviour(SpellBehaviourOneShot parent)
