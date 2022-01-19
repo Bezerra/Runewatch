50bf8d3fd6f4093ff65d544fd567af354ef3260b
pmarques93 <pmarques93@gmail.com>
2022-01-19 15:22:28 +0000

    Disable door minimap icons on start.

--- Arcane Ascension/Assets/Scripts/Minimap/MinimapActivation.cs
+++ Arcane Ascension/Assets/Scripts/Minimap/MinimapActivation.cs
@@ -7,12 +7,25 @@ public class MinimapActivation : MonoBehaviour
 {
     [SerializeField] private MinimapIcon minimapIcon;
 
+    private LevelPiece parentLevelPiece;
+
+    private void Awake() =>
+        parentLevelPiece = GetComponentInParent<LevelPiece>();
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == Layers.PlayerLayerNum ||
             other.gameObject.layer == Layers.InvisiblePlayerLayerNum)
         {
+            // Activates level piece on minimap
             minimapIcon.SetIconActive(true);
+
+            // Activates level piece's doors
+            foreach (Door contactPoint in parentLevelPiece.GetComponentsInChildren<Door>())
+            {
+                contactPoint.GetComponentInChildren<MinimapIcon>(true).SetIconActive(true);
+            }
+
             Destroy(gameObject);
         }
     }
