94bd8439b0084ef0a4068ce47971b63b19469f9d
pmarques93 <pmarques93@gmail.com>
2022-02-08 20:12:31 +0000

    Add loading screen image. Add main menu background video.

--- Arcane Ascension/Assets/TEMP GOES TO GIT/CameraPostProcessOff.cs
+++ Arcane Ascension/Assets/TEMP GOES TO GIT/CameraPostProcessOff.cs
@@ -7,19 +7,28 @@ using UnityEngine.Rendering.Universal;
 /// </summary>
 public class CameraPostProcessOff : MonoBehaviour, IFindPlayer
 {
+    [Header("This is used for procedural generation demonstration scene or for scenes" +
+        " that do not spawn the player")]
+    [SerializeField] private bool keepMainCamPostProcess;
+
     /// <summary>
     /// POST PROCESS IS THE ONE USED ON HANDSCAMERA, MAIN CAMERA SHOULD REMAIN TURNED OFF.
     /// </summary>
     private void Awake()
     {
-        UniversalAdditionalCameraData cameraData = Camera.main.GetUniversalAdditionalCameraData();
+        UniversalAdditionalCameraData cameraData = 
+            Camera.main.GetUniversalAdditionalCameraData();
 
-        cameraData.renderPostProcessing = false;
+        if (keepMainCamPostProcess)
+            PostProcessOn();
+        else
+            cameraData.renderPostProcessing = false;
     }
 
     public void PostProcessOn()
     {
-        UniversalAdditionalCameraData cameraData = Camera.main.GetUniversalAdditionalCameraData();
+        UniversalAdditionalCameraData cameraData = 
+            Camera.main.GetUniversalAdditionalCameraData();
 
         cameraData.renderPostProcessing = true;
     }
