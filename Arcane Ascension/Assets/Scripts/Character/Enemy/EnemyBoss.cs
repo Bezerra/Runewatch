db2ada8138e2e3d0582a473ea5b6a839b8a52695
pmarques93 <pmarques93@gmail.com>
2022-03-03 22:02:02 +0000

    Fix fire demon health on enrage transition.

--- Arcane Ascension/Assets/Scripts/Character/Enemy/EnemyBoss.cs
+++ Arcane Ascension/Assets/Scripts/Character/Enemy/EnemyBoss.cs
@@ -8,6 +8,8 @@ public class EnemyBoss : Enemy
     [SerializeField] private EnemyCharacterSO enragedCharacterSO;
     [Range(0f, 1f)] [SerializeField] private float healthToEnrage = 0.5f;
 
+    private float healthOnEnrageTransition;
+
     /// <summary>
     /// If a value is met, triggers enraged animation + logic.
     /// </summary>
@@ -18,13 +20,28 @@ public class EnemyBoss : Enemy
         if (CommonValues == enragedCharacterSO) return;
         if (EnemyStats.Health / EnemyStats.MaxHealth <= healthToEnrage)
         {
+            // Gets  enemy's current health
+            healthOnEnrageTransition = 0;
+            healthOnEnrageTransition = EnemyStats.Health;
+
             CommonValues = enragedCharacterSO;
+
+            // Resets variables for important classes
             EnemyStats.ResetAll();
-            EnemyStats.Heal(EnemyStats.MaxHealth, StatsType.Health);
             ResetAll();
             Animation.ResetAll();
+
+            // Sets health to the previous state health
+            EnemyStats.Health = healthOnEnrageTransition;
+
+            // Triggers enraged animation
             Animation.TriggerEnragedAnimation();
+
+            // Stops states logic
             StateMachine.Stop();
+
+            // Next state will be trigered after engaged animation is over
+            // on fireDemonEnragedAnimationBehaviour script
         }
     }
 
