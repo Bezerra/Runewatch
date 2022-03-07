e5c6bedf46c2e8265a846e9d9b06f37ddca2b849
pmarques93 <pmarques93@gmail.com>
2022-03-07 23:02:13 +0000

    Update breakable logic. Add brone vases sounds.

--- Arcane Ascension/Assets/Scripts/Interaction/Interaction Spells/Breakable.cs
+++ Arcane Ascension/Assets/Scripts/Interaction/Interaction Spells/Breakable.cs
@@ -6,10 +6,18 @@ using UnityEngine;
 public class Breakable : AbstractInteractionWithSpell
 {
     [SerializeField] private GameObject brokenVase;
+    private GameObject spawnedVase;
+
+    private void Start()
+    {
+        spawnedVase = Instantiate(brokenVase, transform.position, transform.rotation);
+        spawnedVase.SetActive(false);
+        spawnedVase.transform.SetParent(transform.parent);
+    }
 
     protected override void ActionToTake()
     {
-        Instantiate(brokenVase, transform.position, transform.rotation);
-        Destroy(gameObject);
+        gameObject.SetActive(false);
+        spawnedVase.SetActive(true);
     }
 }
