cc52b1ae31af35914d0aae28e83da99a09c51e6c
pmarques93 <pmarques93@gmail.com>
2022-03-15 20:57:59 +0000

    Add catapult golem scrip. objs. characters + stats.

--- Arcane Ascension/Assets/Scripts/Interaction/Interaction Objects/ChestPersistentVFXControl.cs
+++ Arcane Ascension/Assets/Scripts/Interaction/Interaction Objects/ChestPersistentVFXControl.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Turns particles into scaled time after gameobject enabled.
+/// </summary>
 public class ChestPersistentVFXControl : MonoBehaviour
 {
     [Header("ONLY particles that are unscaled by default")]
     [SerializeField] private ParticleSystem[] particleSystems;
     private ParticleSystem.MainModule[] mainModules;
 
-    private Chest chest;
-
     private void Awake()
     {
-        chest = GetComponentInParent<Chest>();
         mainModules = new ParticleSystem.MainModule[particleSystems.Length];
         for (int i = 0; i < particleSystems.Length; i++)
         {
