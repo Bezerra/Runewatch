d530ff4cb44b52d9740acb9e502b63e8855e0328
pmarques93 <pmarques93@gmail.com>
2022-03-16 23:34:18 +0000

    Add grimoire unspent points icon + logic.

---
+++ Arcane Ascension/Assets/Scripts/UI/Main Menu/GrimoireUnspentPoints.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls an icon. Turns it on or off depending if new arcane power points
+/// were obtained.
+/// </summary>
+public class GrimoireUnspentPoints : MonoBehaviour
+{
+    [SerializeField] private GameObject icon;
+    [SerializeField] private GameObject grimoireMenu;
+
+    private CharacterSaveDataController characterSaveData;
+
+    private void Awake()
+    {
+        characterSaveData = FindObjectOfType<CharacterSaveDataController>();
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("EN");
+        if (characterSaveData.SaveData.ArcanePower != 
+            PlayerPrefs.GetInt("ArcanePowerOnLastEnable", 0))
+        {
+            icon.SetActive(true);
+        }
+        else
+        {
+            icon.SetActive(false);
+        }
+
+        PlayerPrefs.SetInt("ArcanePowerOnLastEnable", 
+            characterSaveData.SaveData.ArcanePower);
+    }
+
+    private void Update()
+    {
+        if (grimoireMenu.activeSelf)
+        {
+            if (icon.activeSelf)
+            {
+                icon.SetActive(false);
+            }
+        }
+    }
+}
