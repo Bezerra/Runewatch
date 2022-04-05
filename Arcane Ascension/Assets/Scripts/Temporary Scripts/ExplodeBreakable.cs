0ed37d3eecf982c1ac27f0e4af75691118a31e72
Pedro Bezerra <pabezerra96@gmail.com>
2022-04-05 13:44:38 +0100

    Update vase breaking

---
+++ Arcane Ascension/Assets/Scripts/Temporary Scripts/ExplodeBreakable.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeBreakable : MonoBehaviour
+{
+    [SerializeField] float minForce;
+    [SerializeField] float maxForce;
+    [SerializeField] float radius;
+
+    private void OnEnable()
+    {
+        
+        foreach (Transform t in transform)
+        {
+            var rb = t.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                print("explode");
+                rb.AddExplosionForce(Random.Range(minForce, maxForce), transform.position, radius);
+            }
+        }
+    }
+}
