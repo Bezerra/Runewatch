5885e6383de6ccc2ab27cf75cebd2386fa12a440
pmarques93 <pmarques93@gmail.com>
2022-04-07 22:17:56 +0100

    Add initial achievments data struct.

---
+++ Arcane Ascension/Assets/Scripts/Save System/Data Structs/AchievmentsSaveData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+/// <summary>
+/// Struct responsible for keeping achievments save data.
+/// </summary>
+public struct AchievmentsSaveData 
+{
+    // Public fields for JSON
+    public int EnemiesKilled;
+    public int ArcanePowerObtained;
+    public float DamageDone;
+    public float DamageTaken;
+    public float[] RunTime;
+}
--- Arcane Ascension/Assets/Scripts/Save System/RunSaveData.cs
+++ Arcane Ascension/Assets/Scripts/Save System/RunSaveData.cs
@@ -6,11 +6,13 @@ public class RunSaveData: AbstractSaveData
     // Public fields for JSON
     public PlayerSaveData PlayerSavedData;
     public DungeonSaveData DungeonSavedData;
+    public AchievmentsSaveData AchievmentsSaveData;
     public string Difficulty;
 
     public RunSaveData()
     {
         PlayerSavedData = new PlayerSaveData();
         DungeonSavedData = new DungeonSaveData();
+        AchievmentsSaveData = new AchievmentsSaveData();
     }
 }
