0bfd190193b5940fc17b14babd806356c8effbed
Pedro Bezerra <pabezerra96@gmail.com>
2022-04-10 14:21:46 +0100

    Update Dissolve colors for Small Golems

---
+++ Arcane Ascension/Assets/Scripts/Temporary Scripts/VFXElementsColors.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFXElementsColors : MonoBehaviour
+{
+    [SerializeField] Color ignis;
+    [SerializeField] Color fulgur;
+    [SerializeField] Color aqua;
+    [SerializeField] Color terra;
+    [SerializeField] Color natura;
+    [SerializeField] Color Lux;
+    [SerializeField] Color umbra;
+}
