cf79d6e1e8545f6267bfea4e9414d12fdfa69a56
MrVerdinsky <goncalo.vila.verde@hotmail.com>
2022-04-16 08:40:06 +0100

    Add SpellCard UI / Fix SpellBar BG / Fix Convex issue with broken vases

--- Arcane Ascension/Assets/Scripts/UI/Upgrades In Game/AbilitySpellCardText.cs
+++ Arcane Ascension/Assets/Scripts/UI/Upgrades In Game/AbilitySpellCardText.cs
@@ -7,14 +7,18 @@ using TMPro;
 /// </summary>
 public class AbilitySpellCardText : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI spellTierText;
+    [SerializeField] private Image spellTierGem;
+    [SerializeField] private Sprite[] TierGems;
     [SerializeField] private TextMeshProUGUI spellDamageText;
+    [SerializeField] private TextMeshProUGUI spellDamageBehaviour;
     [SerializeField] private Image spellImage;
     [SerializeField] private TextMeshProUGUI spellNameText;
     [SerializeField] private TextMeshProUGUI spellMana;
     [SerializeField] private TextMeshProUGUI spellDescriptionText;
-    [SerializeField] private Image spellTargetImage;
-    [SerializeField] private Image spellElementImage;
+    [SerializeField] private Image spellElementGem;
+    [SerializeField] private Sprite[] ElementGems;
+    [SerializeField] private ScriptableObject DmgBehaviour;
+
 
     [Header("Empty card gameobject")]
     [SerializeField] private GameObject emptyCard;
@@ -25,15 +29,72 @@ public class AbilitySpellCardText : MonoBehaviour
     /// <param name="spell"></param>
     public void UpdateInfo(ISpell spell)
     {
-        spellTierText.text = spell.Tier.ToString();
+        
+        
         spellDamageText.text = 
             Mathf.Floor(spell.MinMaxDamage.x).ToString() + " - " + Mathf.Floor(spell.MinMaxDamage.y).ToString();
         spellImage.sprite = spell.Icon;
         spellNameText.text = spell.Name;
         spellMana.text = spell.ManaCost.ToString();
         spellDescriptionText.text = spell.Description;
-        spellTargetImage.sprite = spell.TargetTypeIcon;
-        spellElementImage.sprite = spell.ElementIcon;
+
+        if (spell.DamageBehaviour == DmgBehaviour)
+        {
+            spellDamageBehaviour.text = "Area of Effect";
+        }
+        else if (spell.DamageBehaviour == null)
+        {
+            spellDamageBehaviour.text = "Self";
+
+        }
+        else
+            spellDamageBehaviour.text = "Single Target";
+
+
+        switch (spell.Element)
+
+        {
+            case ElementType.Ignis:
+                spellElementGem.sprite = ElementGems[1];
+                break;
+            case ElementType.Aqua:
+                spellElementGem.sprite = ElementGems[2];
+                break;
+            case ElementType.Terra:
+                spellElementGem.sprite = ElementGems[3];
+                break;
+            case ElementType.Fulgur:
+                spellElementGem.sprite = ElementGems[4];
+                break;
+            case ElementType.Natura:
+                spellElementGem.sprite = ElementGems[5];
+                break;
+            case ElementType.Lux:
+                spellElementGem.sprite = ElementGems[6];
+                break;
+            case ElementType.Umbra:
+                spellElementGem.sprite = ElementGems[7];
+                break;
+            default:
+                spellElementGem.sprite = ElementGems[0];
+                break;
+        }
+        
+        switch (spell.Tier)
+        {
+            case 1:
+                spellTierGem.sprite = TierGems[0];
+                break;
+            case 2:
+                spellTierGem.sprite = TierGems[1];
+                break;
+            case 3:
+                spellTierGem.sprite = TierGems[2];
+                break;
+            default:
+                spellTierGem.sprite = TierGems[3];
+                break;
+        }
     }
 
     public void ShowEmptyCard(bool show)
